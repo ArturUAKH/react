a6a86c4673972e1aaf11207bb4eca5c1f5a9acce
Artur <arti404049@gmail.com>
2023-04-14 09:55:01 +0300

    addTodo

--- react-lesson2/src/components/todo-list/task/task.js
+++ react-lesson2/src/components/todo-list/task/task.js
@@ -7,7 +7,7 @@ function Task ({task}){
         <tr>
             <td>{id}</td>
             <td>{title}</td>
-            <td>{completed? 'Завдання виконане' : 'Позначити виконаним?'}</td>
+            <td>{completed? "Успішно виконано ✅" : <button type="button">позначити виконаним  ✍️</button>}</td>
             <td>{`${date.getFullYear()}-${date.getMonth()+1 > 10 ? date.getMonth()+1 : "0" + (date.getMonth()+1)}-${date.getDate()}`}</td>
         </tr>
        
