528f3d8496a7eb1eb9d93a0317f9024217199708
Artur <arti404049@gmail.com>
2023-07-19 19:28:13 +0300

    CREATE: EmployeesApp + layout

---
+++ react-lesson3/src/components/app/app.js
@@ -0,0 +1,28 @@
+import AppFilter from "../app-filter/app-filter";
+import AppInfo from "../app-info/app-info";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+import EmployeesList from "../employees-list/employees-list";
+import SearchPanel from "../search-panel/search-panel";
+import "./app.css";
+
+function App() {
+	let data = [
+		{ name: "Alex", salary: 800, increase: false, id: 1 },
+		{ name: "Bob", salary: 1500, increase: true, id: 2 },
+		{ name: "Cristine", salary: 3000, increase: false, id: 3 },
+	];
+
+	return (
+		<div className="app">
+			<AppInfo />
+			<div className="search-panel">
+				<SearchPanel />
+				<AppFilter />
+			</div>
+			<EmployeesList data={data} />
+			<EmployeesAddForm />
+		</div>
+	);
+}
+
+export default App;
