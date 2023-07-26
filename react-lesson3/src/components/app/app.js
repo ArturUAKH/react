316c8e85f7f9842dfc6a2a25b0fe9f21f41248e1
Artur <arti404049@gmail.com>
2023-07-26 22:31:48 +0300

    ADD:SearchPanel functional

--- react-lesson3/src/components/app/app.js
+++ react-lesson3/src/components/app/app.js
@@ -17,6 +17,8 @@ class App extends Component {
 				{ name: "Bob", salary: 1500, increase: true, id: 2 },
 				{ name: "Cristine", salary: 3000, increase: false, id: 3 },
 			],
+			term: "",
+			filter: "all",
 		};
 		this.maxId = 4;
 	}
@@ -57,18 +59,45 @@ class App extends Component {
 		}));
 	};
 
+	onUpdateSearch = term => {
+		this.setState({ term });
+	};
+
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1;
+		});
+	};
+
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case "rise":
+				return items.filter(item => item.rise);
+			case "moreThan1000":
+				return items.filter(item => item.salary > 1000);
+			default:
+				return items;
+		}
+	};
+
 	render() {
+		const { data, term, filter } = this.state;
 		const employees = this.state.data.length;
 		const increased = this.state.data.filter(item => item.increase).length;
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 		return (
 			<div className="app">
 				<AppInfo employees={employees} increased={increased} />
 				<div className="search-panel">
-					<SearchPanel />
+					<SearchPanel onUpdateSearch={this.onUpdateSearch} />
 					<AppFilter />
 				</div>
 				<EmployeesList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItem}
 					onToggleProp={this.onToggleProp}
 				/>
